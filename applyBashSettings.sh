6ba9a33e4761b8069b791902c5cffef00d047e29
PyaBzz <>
2020-04-15 22:54:00 +0100

    Check if backup exists

--- applyBashSettings.sh
+++ applyBashSettings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ~/.bashrc ./source/.bashrc-bak
-echo "Backup created in ./source/.bashrc-bak"
+cp ~/.bashrc ./source/bashrc-bak
+echo "Backup created in ./source/bashrc-bak"
 
 cat ./source/bashrc.txt >> ~/.bashrc
 echo "Bash Mod Applied!"
--- restoreBashSettings.sh
+++ restoreBashSettings.sh
@@ -1,3 +1,3 @@
-cp ./source/.bashrc-bak ~/.bashrc
+cp ./source/bashrc-bak ~/.bashrc
 echo "Backup of .bashrc restored!"
 
