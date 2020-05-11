abfb54205287c307c9c7599a173e4afddf1b9d66
pyabzz <>
2020-05-11 17:51:23 +0100

    Make confirmation logic reusable

--- applyMod.sh
+++ applyMod.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
-function bashMod {
- if [ -f bashrc/backup ]; then
-  echo "Backup file exists!"
-  read -p "Overwrite it? (Y/N): " confirm
+function askUser {
+ local res=$1
+ if [ -z "$2" ]
+ then
+  local confirm
+  read -p "Proceed? (Y/N): " confirm
+ else
+  read -p "$2 (Y/N): " confirm
+ fi
 
-  if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+ if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
+ then
+  eval $res=true
+ else
+  eval $res=false
+ fi
+}
+
+function applyBazMod {
+ if [ -f bashrc/backup ];
+ then
+  echo "Backup file exists!"
+  local shouldOverWrite
+  askUser shouldOverWrite "Shall we?"
+  echo $shouldOverWrite
+  if [ $shouldOverWrite == true ]; then
     echo "Overwriting backup file ..."
   else
     return
   fi
  fi
 
- cp ~/.bashrc ./bashrc/backup
- echo "Backup saved in ./bashrc/backup"
+cp ~/.bashrc ./bashrc/backup
+echo "Backup saved in ./bashrc/backup"
 
- cat ./bashrc/source >> ~/.bashrc
- echo "Bash Mod Applied!"
+cat ./bashrc/source >> ~/.bashrc
+echo "Bash Mod Applied!"
 }
 
-bashMod
+applyBazMod
