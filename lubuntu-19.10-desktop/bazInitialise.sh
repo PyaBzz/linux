3832b38f348f823eb9f3b3df896afcb5e461a0a1
pyabzz <>
2020-05-11 13:47:00 +0100

    Add Lubuntu initialisation script

---
+++ lubuntu-19.10-desktop/bazInitialise.sh
@@ -0,0 +1,30 @@
+
+#=================  Uninstall  =================
+
+apt-get purge libreoffice*
+apt-get purge 2048-qt
+apt-get purge qtpass
+apt-get purge qlipper
+apt-get purge noblenote
+apt-get purge kcalc
+apt-get purge k3b*
+apt-get purge trojita
+apt-get purge pulseaudio*
+apt-get purge screengrab
+apt-get purge skanlite
+apt-get purge lximage-qt
+apt-get purge vlc*
+apt-get purge transmission-*
+apt-get purge qpdfview*
+
+#=================  Cleanup  =================
+
+apt autoremove
+
+#=================  Install  =================
+
+apt-get update
+#apt install git-all
+#git clone http://pyabzz@github.com/pyabzz/bazLinux
+
+
