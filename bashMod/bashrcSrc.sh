542922d4199bedc5c2c2ceec09c7dd06920397e2
PyaBzz <>
2020-05-12 20:43:53 +0100

    Autoformat file

--- bashMod/bashrcSrc.sh
+++ bashMod/bashrcSrc.sh
@@ -7,59 +7,59 @@
 #=============  Function Definitions  ============
 
 function repeatText {
-if [ -z "$2" ]
-then
- for ((i=1; i<=$1; i++))
-  do
-   printf " "
-  done
-else
- for ((i=1; i<=$1; i++))
-  do
-   printf $2
-  done
-fi
+    if [ -z "$2" ]
+    then
+        for ((i=1; i<=$1; i++))
+        do
+            printf " "
+        done
+    else
+        for ((i=1; i<=$1; i++))
+        do
+            printf $2
+        done
+    fi
 }
 
 function printGreeting {
-
- local paddingLen=20
- local text="  Welcome $USER  "
- local textLen=${#text}
- local lineLen=2*paddingLen+textLen
-
- echo
- repeatText lineLen "#" && echo
- repeatText lineLen "#" && echo
- repeatText paddingLen "#" && repeatText textLen && repeatText paddingLen "#" && echo
- repeatText paddingLen "#" && printf "  Welcome $USER  " && repeatText paddingLen "#" && echo
- repeatText paddingLen "#" && repeatText textLen && repeatText paddingLen "#" && echo
- repeatText lineLen "#" && echo
- repeatText lineLen "#" && echo
- echo
-
+    
+    local paddingLen=20
+    local text="  Welcome $USER  "
+    local textLen=${#text}
+    local lineLen=2*paddingLen+textLen
+    
+    echo
+    repeatText lineLen "#" && echo
+    repeatText lineLen "#" && echo
+    repeatText paddingLen "#" && repeatText textLen && repeatText paddingLen "#" && echo
+    repeatText paddingLen "#" && printf "  Welcome $USER  " && repeatText paddingLen "#" && echo
+    repeatText paddingLen "#" && repeatText textLen && repeatText paddingLen "#" && echo
+    repeatText lineLen "#" && echo
+    repeatText lineLen "#" && echo
+    echo
+    
 }
 
 function setPromptStyle {
- local e='\[\033['  # Escape sequence
- export PS1="${e}95m\]\u${e}91m\]@${e}96m\]\h:${e}94m\]\w${e}93m\]$ ${e}0m\]"
+    local e='\[\033['  # Escape sequence
+    export PS1="${e}95m\]\u${e}91m\]@${e}96m\]\h:${e}94m\]\w${e}93m\]$ ${e}0m\]"
 }
 
 function postInUpRightCorner {
- local len=${#1};
- local colour='\033[30;42m';
-
- tput sc;
- tput cup 0 $(($(tput cols)-len));
- printf "${colour}$1";
- tput rc;
+    local len=${#1};
+    local colour='\033[30;42m';
+    
+    tput sc;
+    tput cup 0 $(($(tput cols)-len));
+    printf "${colour}$1";
+    tput rc;
 }
 
 
 function showDateTime {
- while sleep 1;do
-  postInUpRightCorner "$(date +"%F @ %T")";
- done
+    while sleep 1;do
+        postInUpRightCorner "$(date +"%F @ %T")";
+    done
 }
 
 #=====================  Apply  ===================
