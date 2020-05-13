5c236b3c6ad08af487e1c877236c674c4b62a1d3
pyabzz <>
2020-05-13 20:00:51 +0100

    Add Chrome and Dropbox

--- lubuntu-19.10/01-install.sh
+++ lubuntu-19.10/01-install.sh
@@ -8,13 +8,42 @@ git
 do
     clear
     printLine 60 "#"
-    echo "Installing $packageName"
-    sudo apt-get install $packageName
+    if (askUser "Install $packageName?") then
+        echo "Installing $packageName"
+        sudo apt-get install $packageName
+    else
+        echo "Skipped $packageName ..."
+    fi
 done
 
 #_________________  Snap  _________________
 
 clear
 printLine 60 "#"
-echo "Installing VisualStudio Code"
-sudo snap install --classic code
+if (askUser "Install VisualStudio Code?") then
+    echo "Installing VisualStudio Code"
+    sudo snap install --classic code
+else
+    echo "Skipped VisualStudio Code ..."
+fi
+
+#_________________  Direct Download  _________________
+
+clear
+printLine 60 "#"
+if (askUser "Install Google Chrome?") then
+    wget -O ~/Downloads/GoogleChrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install ~/Downloads/GoogleChrome.deb
+else
+    echo "Skipped Google Chrome ..."
+fi
+
+clear
+printLine 60 "#"
+if (askUser "Install Dropbox?") then
+    wget -O ~/Downloads/Dropbox.tar.gz "https://www.dropbox.com/download?plat=lnx.x86_64"
+    tar xzf ~/Downloads/Dropbox.tar.gz -C ~
+    ~/.dropbox-dist/dropboxd
+else
+    echo "Skipped Dropbox ..."
+fi
