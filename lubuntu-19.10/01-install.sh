5fff00f277ead3057a3cf9f03983025de1ca8db9
pyabzz <>
2020-05-19 21:33:22 +0100

    Add MS-SQL

--- lubuntu-19.10/01-install.sh
+++ lubuntu-19.10/01-install.sh
@@ -43,6 +43,20 @@ else
     echo "Skipped VisualStudio Code ..."
 fi
 
+#_________________  Misc Repos  _________________
+
+clear
+printLine 60 "#"
+if (askUser "Install MS SQL server?") then
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)"
+    sudo apt-get update
+    sudo apt-get install -y mssql-server
+    sudo /opt/mssql/bin/mssql-conf setup
+else
+    echo "Skipped Google Chrome ..."
+fi
+
 #_________________  Direct Download  _________________
 
 clear
