f3e5ed7ec40472cf10019402d5b5d121c5de3959
pyabzz <>
2020-05-24 22:09:57 +0100

    Add VirtualBox

--- lubuntu-19.10/01-install.sh
+++ lubuntu-19.10/01-install.sh
@@ -57,6 +57,18 @@ else
     echo "Skipped Google Chrome ..."
 fi
 
+clear
+printLine 60 "#"
+packageName="VirtualBox"
+if (askUser "Install $packageName?"); then
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+    sudo apt update
+    sudo apt install virtualbox
+else
+    echo "Skipped $packageName ..."
+fi
+
 #_________________  Direct Download  _________________
 
 clear
