11ffdba3cf60e5ee56dbc914c6ec86fa8c2f9f47
pyabzz <>
2020-06-01 22:43:42 +0100

    Add avatar to SDDM login

--- lubuntu-19.10/03-config.sh
+++ lubuntu-19.10/03-config.sh
@@ -10,8 +10,9 @@ fi
 
 step="SDDM customisation"
 if (askUser "Apply $step?"); then
-    sudo cp source/theme.conf /usr/share/sddm/themes/lubuntu
-    sudo cp source/diamondPlate.jpg /usr/share/sddm/themes/lubuntu
+    sudo cp source/SDDM/theme.conf /usr/share/sddm/themes/lubuntu
+    sudo cp source/SDDM/diamondPlate.jpg /usr/share/sddm/themes/lubuntu
+    cp source/SDDM/.face.icon ~
 else
     echo "Skipped $step ..."
 fi
