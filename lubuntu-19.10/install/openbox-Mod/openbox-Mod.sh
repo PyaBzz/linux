f535a1d340a4621ab21cb55b593d1564f9f02471
pyabzz <>
2020-06-27 23:45:58 +0100

    Reorganise

--- lubuntu-19.10/install/openbox-Mod/openbox-Mod.sh
+++ lubuntu-19.10/install/openbox-Mod/openbox-Mod.sh
@@ -1,19 +1,19 @@
-source ../../bashSource/functions.sh
+source ../../../bashSource/functions.sh
 
 function applyOpenboxMod() {
-    if (fileExists ../source/Openbox/lxqt-rc.bak); then
+    if (fileExists ./lxqt-rc.bak); then
         echo "Backup file exists!"
         if (askUser "Overwrite it?"); then
             echo "Overwriting backup file ..."
         else
-            echo "Skipping $packageName ..."
+            echo "Skipped $packageName"
             return
         fi
     fi
     echo "Applying $packageName"
-    cp ~/.config/openbox/lxqt-rc.xml ../source/Openbox/lxqt-rc.bak
-    echo "Backup saved in ../source/Openbox/lxqt-rc.bak"
-    cp ../source/Openbox/lxqt-rc.xml ~/.config/openbox/lxqt-rc.xml
+    cp ~/.config/openbox/lxqt-rc.xml ./lxqt-rc.bak
+    echo "Backup saved in ./lxqt-rc.bak"
+    cp ./lxqt-rc.xml ~/.config/openbox/lxqt-rc.xml
     openbox --reconfigure
     echo "$packageName Applied!"
 }
