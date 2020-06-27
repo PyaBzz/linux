ef8a1d3936fbd10d2ae2f6c47976079969c5a09e
pyabzz <>
2020-06-27 22:55:40 +0100

    Add confirmation whether to apply config

--- lubuntu-19.10/install/git.sh
+++ lubuntu-19.10/install/git.sh
@@ -8,12 +8,16 @@ packageName="git"
 if (askUser "Install $packageName?"); then
     echo "Installing $packageName"
     sudo apt-get install $packageName
-    git config --global user.name pyabzz
-    git config --global user.email ""
-    #===================  Add Aliases  ===================
-    echo "alias gistatus='clear && git status'" >> ../../bashMod/bashrcSrcDynamic.sh
-    echo "alias giff='clear && git diff'" >> ../../bashMod/bashrcSrcDynamic.sh
-    echo "alias gadd='clear && git add -p'" >> ../../bashMod/bashrcSrcDynamic.sh
+
+    if (askUser "Apply config?"); then
+        git config --global user.name pyabzz
+        git config --global user.email ""
+        echo "alias gistatus='clear && git status'" >>../../bashMod/bashrcSrcDynamic.sh
+        echo "alias giff='clear && git diff'" >>../../bashMod/bashrcSrcDynamic.sh
+        echo "alias gadd='clear && git add -p'" >>../../bashMod/bashrcSrcDynamic.sh
+    else
+        echo "Skipped config"
+    fi
 else
     echo "Skipped $packageName"
 fi
