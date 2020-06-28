e76baf289652daea406e114cdec5684daaa42675
pyabzz <>
2020-06-28 11:55:17 +0100

    Reorganised dropbox

--- lubuntu-19.10/dropbox/dropbox.sh
+++ lubuntu-19.10/dropbox/dropbox.sh
@@ -1,4 +1,4 @@
-source ../bashSource/functions.sh
+source ../../bashSource/functions.sh
 
 clear
 printLine 60 "#"
@@ -6,7 +6,7 @@ packageName="Dropbox"
 if (askUser "Install $packageName"); then
     wget "https://www.dropbox.com/download?plat=lnx.x86_64" -O ~/Downloads/Dropbox.tar.gz
     tar xzf ~/Downloads/Dropbox.tar.gz -C ~
-    cp ../source/dropbox.desktop ~/Desktop
+    cp ./dropbox.desktop ~/Desktop
 else
     echo "Skipped $packageName"
 fi
