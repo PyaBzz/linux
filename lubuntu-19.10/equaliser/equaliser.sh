6e40343ebf5a5afe0bcd73aca4731f6d02ca00f5
pyabzz <>
2020-06-29 10:37:37 +0100

    Bugfix

--- lubuntu-19.10/equaliser/equaliser.sh
+++ lubuntu-19.10/equaliser/equaliser.sh
@@ -1,16 +1,15 @@
 source ../../bashSource/functions.sh
 
-sudo apt-get update
-
 clear
 printLine 60 "#"
 packageName="pulseaudio-equalizer"
 
 if (askUser "Install $packageName?"); then
+    sudo apt-get update
     sudo apt-get install $packageName
     cp ./Equaliser.desktop ~/Desktop
-    echo "" >>/etc/pulse/default.pa
-    echo "load-module module-dbus-protocol" >>/etc/pulse/default.pa
+    echo "" | sudo tee -a /etc/pulse/default.pa >/dev/null
+    echo "load-module module-dbus-protocol" | sudo tee -a /etc/pulse/default.pa >/dev/null
 else
     echo "Skipped $packageName"
 fi
