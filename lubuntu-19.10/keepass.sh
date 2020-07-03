c7aa3a184477f1b7224f73026e252c6f8a333ba3
pyabzz <>
2020-07-03 12:26:12 +0100

    Fix

--- lubuntu-19.10/keepass.sh
+++ lubuntu-19.10/keepass.sh
@@ -1,12 +1,11 @@
 source ../bashSource/functions.sh
 
-sudo apt update
-
 clear
 printLine 60 "#"
 packageName="keepass2"
 
 if (askUser "Install $packageName?"); then
+    sudo apt update
     echo "Installing $packageName"
     sudo apt install $packageName
 else
