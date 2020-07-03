c0d4ca553518829d5f8d9abdfba8fed4f1b2cc0f
pyabzz <>
2020-07-03 12:16:28 +0100

    Add cleanup step to dropbox installation

--- lubuntu-19.10/dropbox/dropbox.sh
+++ lubuntu-19.10/dropbox/dropbox.sh
@@ -7,6 +7,7 @@ if (askUser "Install $packageName"); then
     wget "https://www.dropbox.com/download?plat=lnx.x86_64" -O ~/Downloads/Dropbox.tar.gz
     tar xzf ~/Downloads/Dropbox.tar.gz -C ~
     cp ./dropbox.desktop ~/Desktop
+    rm ~/Downloads/Dropbox.tar.gz
 else
     echo "Skipped $packageName"
 fi
