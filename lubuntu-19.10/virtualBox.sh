2b5ab9b97b58ace6d6367676d3242c4ce6366793
pyabzz <>
2020-07-05 21:15:27 +0100

    Cleanup

--- lubuntu-19.10/virtualBox.sh
+++ lubuntu-19.10/virtualBox.sh
@@ -1,11 +1,10 @@
 source ../../bashSource/functions.sh
 
-sudo apt-get update
-
 clear
 printLine 60 "#"
 packageName="virtualbox"
 if (askUser "Install $packageName?"); then
+
     wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
     sudo apt update
