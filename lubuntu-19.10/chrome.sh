a80e75583efd65d52f738eb86687458b5a5198b2
pyabzz <>
2020-10-14 01:28:57 +0100

    Set Chrome as default browser

--- lubuntu-19.10/chrome.sh
+++ lubuntu-19.10/chrome.sh
@@ -7,6 +7,9 @@ if (askUser "Install $packageName?"); then
     wget -O ~/Downloads/GoogleChrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo apt install ~/Downloads/GoogleChrome.deb
     rm -rf ~/Downloads/GoogleChrome.deb
+    
+    # Set Chrome as your default browser
+    sed -i "/\[Environment\]/a BROWSER=/usr/bin/google-chrome" ~/.config/lxqt/session.conf
 else
     echo "Skipped $packageName ..."
 fi
