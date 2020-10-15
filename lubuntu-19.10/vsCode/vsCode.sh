ff0cf59c6187bc24e4b77bac48f117e5f5de7ab4
pyabzz <>
2020-10-15 12:07:32 +0100

    Consider existing settings and backup files in installation of VSCode

--- lubuntu-19.10/vsCode/vsCode.sh
+++ lubuntu-19.10/vsCode/vsCode.sh
@@ -3,21 +3,45 @@ source ../../bashSource/functions.sh
 clear
 printLine 60 "#"
 packageName="VS Code"
+
 if (askUser "Install $packageName?"); then
     echo "Installing $packageName"
     sudo snap install --classic code
-
-    if (askUser "Apply config?"); then
-        if (dirMissing ~/.config/Code); then
-            mkdir ~/.config/Code
-        fi
-        if (dirMissing ~/.config/Code/User); then
-            mkdir ~/.config/Code/User
-        fi
-        cp ./settings.json ~/.config/Code/User
-    else
-        echo "Skipped config"
-    fi
 else
     echo "Skipped $packageName"
+fi
+
+if (askUser "Apply config?"); then
+    settingFilePath=~/.config/Code/User/settings.json
+    backupFilePath=~/.config/Code/User/settings.json.bazbak
+    if (dirMissing ~/.config/Code); then
+        mkdir ~/.config/Code
+    fi
+    if (dirMissing ~/.config/Code/User); then
+        mkdir ~/.config/Code/User
+    fi
+
+    if (fileExists $settingFilePath); then
+        echo "Setting file exists!"
+
+        if (fileExists $backupFilePath); then
+            echo "Backup file exists!"
+
+            if (askUser "Overwrite the backup with a new one?"); then
+                echo "Overwriting backup file ..."
+                cp $settingFilePath $backupFilePath
+                echo "Backup saved in $backupFilePath"
+            else
+                echo "Leaving backup file untouched"
+            fi
+        else
+            cp $settingFilePath $backupFilePath
+            echo "Backup saved in $backupFilePath"
+        fi
+    fi
+
+    cp ./settings.json $settingFilePath
+    echo "Settings applied!"
+else
+    echo "Skipped config"
 fi
