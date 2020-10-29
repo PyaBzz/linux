f8ef695275361068e457cbb3bd72f7de5e1642d0
pyabzz <>
2020-10-29 22:26:08 +0000

    Update SDDM

---
+++ lubuntu-19.10/sddm/sddmMod.sh
@@ -0,0 +1,22 @@
+source ../../bashMod/functions.sh
+
+package="SDDM customisation"
+
+myIconFile=./baz.icon
+myConfigFile=./theme.conf
+myImageFile=./bazWallpaper.jpg
+
+targetIconFile=~/.face.icon
+targetConfigFile=/usr/share/sddm/themes/lubuntu/theme.conf
+targetImageFile=/usr/share/sddm/themes/lubuntu/bazWallpaper
+
+clear
+printLine 60 "#"  #Todo: Refactor to askUserClear
+if (askUser "Apply $package?"); then
+    copyOrReplaceFile $myConfigFile to $targetConfigFile
+    copyOrReplaceFile $myImageFile to $targetImageFile
+    copyOrReplaceFile $myIconFile to $targetIconFile
+    echo "Applied $package"
+else
+    echo "Skipped $package"
+fi
