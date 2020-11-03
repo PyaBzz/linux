ff6cb6a1063f4cfba384041b9dd1a37bf45fcb8a
pyabzz <>
2020-11-03 21:16:28 +0000

    Add askUserChoice - incomplete

--- bashExtension/modules/userInteractions.sh
+++ bashExtension/modules/userInteractions.sh
@@ -13,10 +13,26 @@ askUser() {
     else
         read -p "$1 (Y/N): " confirm
     fi
-    
+
     if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
         true
     else
         false
     fi
+}
+
+askUserChoice() {
+    # synopsis: askUserChoice
+    echo $1
+    local index=1
+    local choiceCount=$#
+
+    echo $choiceCount
+
+    for param in "$@"; do
+        if [[ $index != 1 ]]; then
+            echo "$index: $param"
+            ((index++))
+        fi
+    done
 }
