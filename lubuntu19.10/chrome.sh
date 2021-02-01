dedcd794a3b20ecfa23558368e7696826fbe431b
pyabzz <>
2021-02-01 01:48:51 +0000

    Replace tilde with keyword

--- lubuntu19.10/chrome.sh
+++ lubuntu19.10/chrome.sh
@@ -3,12 +3,12 @@ source ../bashExtension/imports.sh
 packageName="Google Chrome"
 
 if (askUserClear "Install $packageName?"); then
-    wget -O ~/Downloads/GoogleChrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install ~/Downloads/GoogleChrome.deb
-    rm -rf ~/Downloads/GoogleChrome.deb
+    wget -O $HOME/Downloads/GoogleChrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo apt install $HOME/Downloads/GoogleChrome.deb
+    rm -rf $HOME/Downloads/GoogleChrome.deb
 
     # Set Chrome as your default browser
-    addTextInFileAfterMarker "BROWSER=/usr/bin/google-chrome" ~/.config/lxqt/session.conf "\[Environment\]"
+    addTextInFileAfterMarker "BROWSER=/usr/bin/google-chrome" $HOME/.config/lxqt/session.conf "\[Environment\]"
 else
     echo "Skipped $packageName ..."
 fi
