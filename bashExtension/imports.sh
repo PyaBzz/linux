6dd9ba285563d984d280229ab558555f4d1440e8
pyabzz <>
2021-02-02 16:58:44 +0000

    Make getMyDir truly reusable

--- bashExtension/imports.sh
+++ bashExtension/imports.sh
@@ -1,5 +1,7 @@
-# Copy this function to any script instead of sourcing it
-getMyDir() { echo "$(readlink -f "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)")"; }
+shopt -s expand_aliases
+# This gets the absolute directory where the script is running
+# Don't make it a func as it would always get directory of the func source
+alias getMyDir='echo "$(readlink -f "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)")"'
 
 sourceFromDir() {
     # synopsis: sourceFromDir <PathToDir>
@@ -7,6 +9,5 @@ sourceFromDir() {
     for f in $1/*.sh; do source $f; done
 }
 
-importsDir=$(getMyDir)
-modulesDir=$importsDir/modules
+modulesDir=$(getMyDir)/modules
 sourceFromDir $modulesDir
--- bashExtension/importsForTesting.sh
+++ bashExtension/importsForTesting.sh
@@ -1,8 +1,9 @@
-# Copy this function to any script instead of sourcing it
-getMyDir() { echo "$(readlink -f "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)")"; }
+shopt -s expand_aliases
+# This gets the absolute directory where the script is running
+# Don't make it a func as it would always get directory of the func source
+alias getMyDir='echo "$(readlink -f "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)")"'
 
-myDir=$(getMyDir)
-source "$myDir/imports.sh"
+source $(getMyDir)/imports.sh
 
 testPassed="${colourGreen}Passed${colourNc}"
 testFailed="${colourRed}Failed${colourNc}"
@@ -13,5 +14,5 @@ printTestTodo() {
 
 printTestName() {
     local testName=$1
-    echo -e "${colourCyan}${f}${colourNc}"
+    echo -e "$colourCyan$f$colourNc"
 }
