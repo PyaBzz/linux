6d58dbb9204dc396177b4655039a5ed13beab0a8
pyabzz <>
2021-02-04 16:16:52 +0000

    Update persian keyboard

--- lubuntu19.10/persianKeyboard/persianKeyboard.sh
+++ lubuntu19.10/persianKeyboard/persianKeyboard.sh
@@ -1,29 +1,29 @@
 source ../../source.sh
 
 apply() {
-    backUp $targetFilePath
-
-    if [[ $? == 2 ]]; then 
+    if (isBackedUp $targetFile); then
         if (askUser "You've already been modded. Update?"); then
             echo "Updating $packageName"
         else
             echo "Aborted"
             return
         fi
+    else
+        backUp $targetFile
     fi
 
-    copyFile $myFilePath to $targetFilePath # Todo: Verify all files exist (including targets to be overwritten) before making any change to the computer
-    # setxkbmap -model pc105 -layout gb,ir -option grp:switch,grp:alt_shift_toggle
+    copyFile $myFile to $targetFile
+    setxkbmap -model pc105 -layout gb,ir -option grp:switch,grp:alt_shift_toggle
     echo "$packageName Applied!"
 }
 
 packageName="persian keyboard layout fix"
-myFilePath=./ir
-targetFilePath=/usr/share/X11/xkb/symbols/ir
+myFile=./ir
+targetFile=/usr/share/X11/xkb/symbols/ir
 
 if [[ $1 == "restore" ]]; then
     if (askUserClear "Undo $packageName?"); then
-        restoreFile $targetFilePath
+        restoreFile $targetFile
     else
         echo "Aborted"
     fi
