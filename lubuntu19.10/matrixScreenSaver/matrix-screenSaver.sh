2b2180cb78377db3f82e27da304740f9e4520dac
pyabzz <>
2021-02-04 11:09:23 +0000

    Update matrixScr

--- lubuntu19.10/matrixScreenSaver/matrix-screenSaver.sh
+++ lubuntu19.10/matrixScreenSaver/matrix-screenSaver.sh
@@ -1,12 +1,14 @@
 source ../../source.sh
 
 packageName="xscreensaver-gl"
+sourceFile=./.xscreensaver
+targetDIr=$HOME
 
 if (askUserClear "Install $packageName?"); then
     sudo apt-get update
     echo "Installing $packageName"
     sudo apt-get install $packageName
-    cp ./.xscreensaver ~
+    copyFile $sourceFile to $targetDIr
 else
     echo "Skipped $packageName"
 fi
