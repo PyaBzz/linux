1502c6398667f36a57a28e9f8afc4e521da4cd91
pyabzz <>
2021-02-06 10:10:04 +0000

    Add -y switch

--- lubuntu20/playerctl.sh
+++ lubuntu20/playerctl.sh
@@ -5,7 +5,7 @@ packageName="playerctl"
 if (askUserClear "Install $packageName?"); then
     sudo apt update
     echo "Installing $packageName"
-    sudo apt install $packageName
+    sudo apt install $packageName -y
 else
     echo "Skipped $packageName"
 fi
