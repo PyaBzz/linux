c6e72bd8fcc9af603149127408fe448d3108ddcb
pyabzz <>
2021-02-16 15:41:12 +0000

    Update netCore

--- lubuntu20/netCore3.1.sh
+++ lubuntu20/netCore3.1.sh
@@ -1,15 +1,18 @@
-source ../../source.sh
+source ../source.sh
 
 packageName=".NET Core SDK 3.1 and runtime"
+downloadUrl="https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb"
+downloadedFile="$HOME/Downloads/packages-microsoft-prod.deb"
 
 if (askUserClear "Install $packageName?"); then
-    wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O ~/Downloads/packages-microsoft-prod.deb
-    sudo dpkg -i ~/Downloads/packages-microsoft-prod.deb
+    wget $downloadUrl -O $downloadedFile
+    sudo dpkg -i $downloadedFile
     sudo apt-get update
-    sudo apt-get install apt-transport-https
+    sudo apt-get install -y apt-transport-https
     sudo apt-get update
-    sudo apt-get install dotnet-sdk-3.1
-    sudo apt-get install dotnet-runtime-3.1
+    sudo apt-get install -y dotnet-sdk-3.1
+    sudo apt-get install -y dotnet-runtime-3.1
+    rm $downloadedFile
 else
     echo "Skipped $packageName"
 fi
