6d1e249e58520bfdabe2ddb3b6c95c4c40093602
pyabzz <>
2021-03-13 19:20:38 +0000

    Small fix

--- lubuntu20/gimp.sh
+++ lubuntu20/gimp.sh
@@ -5,7 +5,7 @@ packageName="gimp"
 if (askUserClear "Install $packageName?"); then
     sudo apt-get update
     echo "Installing $packageName"
-    sudo apt-get install $packageName
+    sudo apt-get install -y $packageName
 else
     echo "Skipped $packageName"
 fi
