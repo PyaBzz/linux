9951dbbe1d3dd451967bd930d602240fa9964545
pyabzz <>
2021-05-20 10:30:20 +0100

    Change screensaver to light weight

--- lubuntu20/matrixScreenSaver/matrix-screenSaver.sh
+++ lubuntu20/matrixScreenSaver/matrix-screenSaver.sh
@@ -1,13 +1,14 @@
 source ../../source.sh
 
-packageName="xscreensaver-gl"
+# packageName="xscreensaver-gl"
+packageName="Matrix Screensaver"
 sourceFile=./.xscreensaver
 targetDIr=$HOME
 
 if (askUserClear "Install $packageName?"); then
-    sudo apt-get update
+    # sudo apt-get update
     echo "Installing $packageName"
-    sudo apt-get install $packageName
+    # sudo apt-get install $packageName
     copyFile $sourceFile to $targetDIr
 else
     echo "Skipped $packageName"
