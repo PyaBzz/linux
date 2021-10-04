2b7e9619942d06cd673d8b3c89ef76d4ef307242
pyabzz <>
2021-10-05 00:12:10 +0100

    Add KegaFusion

---
+++ lubuntu20/kegaFusion/kegaFusion.sh
@@ -0,0 +1,10 @@
+# download the .deb package from https://segaretro.org/Kega_Fusion
+# install it using:
+#  sudo dpkg -i /path/to/deb/file
+#  sudo apt-get install -f
+
+# For config, copy the ini file from here to ~/.Kega\ Fusion/
+# For Shinobi rapid fire, go to "Shortcut Keys" (Global Actions Manager) and bind F11 to the following command
+bash -c 'xdotool getactivewindow keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k sleep 0.0345 keydown l keyup l sleep 0.0345 keydown k keyup k'
+
+# In Shinobi 3 pressing F11 throws many rapid fires
