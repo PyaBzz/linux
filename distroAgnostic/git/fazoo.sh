2b8ac8e687f9d39b7f5d06468dd3ec0d3effa788
pyabzz <>
2021-10-31 22:05:04 +0000

    Add funcs to determine distro

---
+++ distroAgnostic/git/fazoo.sh
@@ -0,0 +1,15 @@
+source ../../source.sh
+
+# if (isArch); then
+#     echo "Yes Arch"
+# else
+#     echo "No arch"
+# fi
+
+# if (isUbuntu); then
+#     echo "Yes Ubun"
+# else
+#     echo "No Ubun"
+# fi
+
+packageManagerUpdate
