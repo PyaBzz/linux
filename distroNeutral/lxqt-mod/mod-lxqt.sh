1dfba2e18f9d4d42e56ab58aef1370eca7cd9917
pyabzz <>
2021-11-04 17:09:19 +0000

    Update lxqt mod

--- distroNeutral/lxqt-mod/mod-lxqt.sh
+++ distroNeutral/lxqt-mod/mod-lxqt.sh
@@ -3,16 +3,36 @@ source ../../source.sh
 packageName="LXQT Mod"
 if (askUserClear "Apply $packageName?"); then
     sourceFiles=(
-        "./session.conf"
-        "./panel.conf"
+        "globalkeyshortcuts.conf"
+        "lxqt-config-file-associations.conf"
+        "lxqt-config-locale.conf"
+        "lxqt-config.conf"
+        "lxqt-powermanagement.conf"
+        "lxqt-runner.conf"
+        "lxqt.conf"
+        "notifications.conf"
+        "panel.conf"
+        "power.conf"
+        "session.conf"
+        "windowmanagers.conf"
     )
 
     targetDir=$HOME/.config/lxqt
     mkdir -p $targetDir
 
     targetFiles=(
-        "$targetDir/session.conf"
+        "$targetDir/globalkeyshortcuts.conf"
+        "$targetDir/lxqt-config-file-associations.conf"
+        "$targetDir/lxqt-config-locale.conf"
+        "$targetDir/lxqt-config.conf"
+        "$targetDir/lxqt-powermanagement.conf"
+        "$targetDir/lxqt-runner.conf"
+        "$targetDir/lxqt.conf"
+        "$targetDir/notifications.conf"
         "$targetDir/panel.conf"
+        "$targetDir/power.conf"
+        "$targetDir/session.conf"
+        "$targetDir/windowmanagers.conf"
     )
 
     backupAndReplaceFiles sourceFiles to targetFiles
