cc63a04935f485483511d604dfceece0931ab7ee
pyabzz <>
2021-11-05 17:48:09 +0000

    Add sddm-mod to distro-neut

---
+++ distroNeutral/mod-sddm/modSddm.sh
@@ -0,0 +1,30 @@
+source ../../source.sh
+
+package="SDDM customisation"
+
+if (askUserClear "Apply $package?"); then
+    :
+else
+    echo "Aborted"
+    exit 1
+fi
+
+# confDir=/etc/sddm.conf.d
+mkDirIfMissing $confDir
+
+sourceFiles=(
+    "./baz.icon"
+)
+# "./theme.conf"
+# "./bazWallpaper.jpg"
+
+targetFiles=(
+    "$HOME/.face.icon"
+)
+# "$confDir/theme.conf"
+# "/usr/share/sddm/themes/lubuntu/bazWallpaper"
+
+copyFiles sourceFiles to targetFiles
+setfacl -m u:sddm:x $HOME
+
+echo "Applied $package"
