e82ff8c48e02fde6604cc5fb7e549e65c40fe9b0
pyabzz <>
2021-11-06 15:17:14 +0000

    Add dropbox to distroNeut

---
+++ distroNeutral/dropbox/dropbox.sh
@@ -0,0 +1,14 @@
+source ../../source.sh
+
+package="Dropbox"
+askToProceed "Install $package?"
+
+downloadUrl="https://www.dropbox.com/download?plat=lnx.x86_64"
+shortcut=./dropbox.desktop
+
+mkDirIfMissing $downloadsDir
+downloadedFile=$downloadsDir/Dropbox.tar.gz
+wget $downloadUrl -O $downloadedFile
+tar xzf $downloadedFile -C $HOME
+copyFile $shortcut to $desktopDir
+rm $downloadedFile
