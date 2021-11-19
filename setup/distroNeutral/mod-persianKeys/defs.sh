709a5fd32dc99e616fce48cfe02b7c70265913fd
pyabzz <>
2021-11-19 20:29:44 +0000

    Add persian keyboard to distroNeut

---
+++ setup/distroNeutral/mod-persianKeys/defs.sh
@@ -0,0 +1,19 @@
+source ../../source.sh
+
+packageName="persian keyboard layout fix"
+myLayoutFile=./ir
+targetLayoutFile=/usr/share/X11/xkb/symbols/ir
+
+apply() {
+    if (isBackedUp $targetLayoutFile); then
+        if (askUser "Overwrite the existing mod?"); then
+            echo "Overwriting mod ..."
+            restoreFile $targetLayoutFile
+        else
+            echo "Skipped"
+            return
+        fi
+    fi
+
+    backupAndReplaceFile $myLayoutFile to $targetLayoutFile
+}
