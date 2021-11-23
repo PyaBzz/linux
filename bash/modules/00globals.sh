4ca46cfbb5751160ea958731b2f04f68180c5e1c
pyabzz <>
2021-11-23 22:39:22 +0000

    Add hostName to global vars

--- bash/modules/00globals.sh
+++ bash/modules/00globals.sh
@@ -3,6 +3,7 @@ bashModDir=$HOME/.bashMod
 aliasDir=$bashModDir/aliases
 desktopDir=$HOME/Desktop
 downloadsDir=$HOME/Downloads
+hostName=$(cat /etc/hostname)
 
 isBashModded() {
     if [ -z ${isAppendageRun+x} ]; then false; else true; fi
