ff95fe69b091330ed523016e802d966f6ff0561c
pyabzz <>
2021-12-06 21:49:05 +0000

    Leftover

--- setup/distroNeutral/mod-lxqt/defs.sh
+++ setup/distroNeutral/mod-lxqt/defs.sh
@@ -2,13 +2,11 @@ source ../../source.sh
 
 lxqtSrcDir="./$hostName/lxqt"
 lxqtSources=($(ls $lxqtSrcDir/*.conf))
-# lxqtTargetDir=$HOME/.config/lxqt
-lxqtTargetDir="./scratchDir/lxqt"
+lxqtTargetDir=$HOME/.config/lxqt
 
 openboxSrcFile="./$hostName/openbox/rc.xml"
-# openboxTargetDir="$HOME/.config/openbox"
-openboxTargetDir="./scratchDir/openbox"
-openboxTargetFile="./scratchDir/openbox/rc.xml"
+openboxTargetDir="$HOME/.config/openbox"
+openboxTargetFile="$openboxTargetDir/rc.xml"
 
 applyToLocal() {
     mkdir -p $lxqtTargetDir
