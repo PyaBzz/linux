dc1f2d06f5828977313725f0275d1bbf95df6994
pyabzz <>
2021-12-07 15:09:42 +0000

    Simplify mod

--- setup/distroNeutral/mod-lxqt/defs.sh
+++ setup/distroNeutral/mod-lxqt/defs.sh
@@ -1,24 +1,26 @@
 source ../../source.sh
 
 lxqtSrcDir="./$hostName/lxqt"
-lxqtSources=($(ls $lxqtSrcDir/*.conf))
 lxqtTargetDir=$HOME/.config/lxqt
 
-openboxSrcFile="./$hostName/openbox/rc.xml"
+openboxSrcDir="./$hostName/openbox"
+openboxSrcFile="$openboxSrcDir/rc.xml"
+
 openboxTargetDir="$HOME/.config/openbox"
 openboxTargetFile="$openboxTargetDir/rc.xml"
 
 applyToLocal() {
     mkdir -p $lxqtTargetDir
-    copyToDirMulti lxqtSources $lxqtTargetDir
+    cp $lxqtSrcDir/*.conf $lxqtTargetDir
 
     mkdir -p $openboxTargetDir
-    copy $openboxSrcFile $openboxTargetFile
+    cp $openboxSrcFile $openboxTargetFile
 }
 
 takeFromLocal() {
     mkdir -p $lxqtSrcDir
     cp $lxqtTargetDir/*.conf $lxqtSrcDir
 
-    copy $openboxTargetFile $openboxSrcFile
+    mkdir -p $openboxSrcDir
+    cp $openboxTargetFile $openboxSrcFile
 }
