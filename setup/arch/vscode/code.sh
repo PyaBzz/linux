7daed08c4ab5de41c4ac2d0b5f0994e64680b9fa
pyabzz <>
2022-05-07 23:01:25 +0100

    Update vscode installer

--- setup/arch/vscode/code.sh
+++
@@ -1,4 +0,0 @@
-# This product file enables official extensions gallery
-# If you like, look for "extensionsGallery" inside the file
-
-sudo cp ./product.json /usr/lib/code
---
+++ setup/arch/vscode/install.sh
@@ -0,0 +1,6 @@
+sudo pacman -Sy
+sudo pacman -Sy code
+
+# This product.json file enables official extensions gallery from Microsoft
+# If you like, look for "extensionsGallery" inside the file
+sudo cp ./product.json /usr/lib/code
