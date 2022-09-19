3356febf27af5e75a7b04aa5a06a91a937dfe3e4
baz <>
2022-09-19 23:07:34 +0100

    Correct installation for vscode

---
+++ setup/arch/vscode/defs.sh
@@ -0,0 +1,18 @@
+source ../source.sh
+
+installVsCode() {
+    mkdir -p $downloadsDir
+    repoLocal=$downloadsDir/vscode
+    mkdir -p $repoLocal
+
+    repoUrl=https://AUR.archlinux.org/visual-studio-code-bin.git
+
+    git clone $repoUrl $repoLocal
+    cd $repoLocal
+    makepkg -sri # automatically runs `pacman -U ...`
+    rm -rf $repoLocal
+}
+
+installKeyRing() {
+    sudo pacman -S gnome-keyring libsecret
+}
