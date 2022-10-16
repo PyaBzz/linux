b129258779e35a64eb362c482f78d674e7da8fba
baz <>
2022-10-16 09:57:43 +0100

    Make persian keyboard more like windows implementation

--- setup/arch/mod-persianKeys/defs.sh
+++ setup/arch/mod-persianKeys/defs.sh
@@ -7,5 +7,7 @@ localLayoutFile=/usr/share/X11/xkb/symbols/ir
 apply() {
     backUp $localLayoutFile
     sudo ln -sf $myLayoutFile $localLayoutFile
-    # To apply layout changes without a restart: setxkbmap -layout ir
+    echo "To apply layout changes without a restart:"
+    echo "setxkbmap -layout ir"
+    echo "setxkbmap -layout gb"
 }
