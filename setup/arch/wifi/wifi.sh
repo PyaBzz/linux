416b2a54d97d029265e2a79da1e4fb86a14cfbdf
baz <>
2022-10-20 19:53:43 +0100

    Add wifi

---
+++ setup/arch/wifi/wifi.sh
@@ -0,0 +1,15 @@
+# sudo pacman -Sy
+
+# sudo pacman -S nm-applet  # The widget in the panel
+# nm-applet 
+
+# sudo pacman -S NetworkManager  # Unsure if this is necessary
+# NetworkManager 
+
+# sudo pacman -S nm-connection-editor  # GUI connection editor
+# nm-connection-editor 
+
+
+# iwconfig      # lists interfaces
+# ip link      # lists interfaces
+# iw dev      # lists interfaces
