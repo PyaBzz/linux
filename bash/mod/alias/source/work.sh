6d6d183177180c666dd8bd7a8104ea2336e29c34
pyabzz <>
2022-12-19 19:48:00 +0000

    Update work aliases

--- bash/mod/alias/source/work.sh
+++ bash/mod/alias/source/work.sh
@@ -1,6 +1,6 @@
-alias daze='date +"year: %Y, month: %m, day: %d, hour: %H, minute: %M" | tee /dev/tty | tr -d "\n" | clip.exe'
+alias daze='date +"year: %Y, month: %m, day: %d, hours: %H, minutes: %M" | tee /dev/tty | tr -d "\n" | clip.exe'
 
-alias dockerps="docker ps --format 'table{{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Ports}}'"
-alias dockeru="docker-compose up -d"
-alias dockerd="docker-compose down"
-alias dockers="docker stop"
+# alias dockerps="docker ps --format 'table{{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Ports}}'"
+# alias dockeru="docker-compose up -d"
+# alias dockerd="docker-compose down"
+# alias dockers="docker stop"
