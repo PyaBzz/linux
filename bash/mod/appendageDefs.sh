fec734f0993f810eee117e4eac78550c3362af2d
pyabzz <>
2023-12-19 18:06:33 +0000

    Add mkcd command

--- bash/mod/appendageDefs.sh
+++ bash/mod/appendageDefs.sh
@@ -31,3 +31,9 @@ setPromptStyle() {
     local e='\[\033[' # Escape sequence
     export PS1="${e}95m\]\u${e}91m\]@${e}96m\]\h:${e}94m\]\w${e}93m\]$ ${e}0m\]"
 }
+
+mkcd ()
+{
+    mkdir -p -- "$1" &&
+    cd -P -- "$1"
+}
